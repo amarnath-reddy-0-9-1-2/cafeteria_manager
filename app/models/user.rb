f4de8042fd6d35c29a1982ce289ec8f44db25471
amarnath-reddy-0-9-1-2 <122003102@sastra.ac.in>
2020-05-01 13:13:08 +0530

    introduced clerk persona

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+
+  def self.get_user_by_email(email)
+    user = all.where("email = ?", email).exists? ? find_by(email: email) : false
+  end
+
   def is_clerk?
     clerk = role == "clerk" ? true : false
   end
@@ -15,9 +21,5 @@ class User < ApplicationRecord
 
   def self.customers
     where("role = ?", "customer")
-  end
-
-  def self.get_user_by_email(email)
-    user = all.where("email = ?", email).exists? ? find_by(email: email) : false
   end
 end
