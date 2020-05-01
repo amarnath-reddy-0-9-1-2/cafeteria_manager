a21521ca591a352b3e9b06649b7f9cb22b8488cf
amarnath-reddy-0-9-1-2 <122003102@sastra.ac.in>
2020-05-01 13:51:51 +0530

    added flash messages

--- app/controllers/order_items_contoller.rb
+++ app/controllers/order_items_contoller.rb
@@ -19,8 +19,10 @@ class OrderItemsController < ApplicationController
     order_item = OrderItem.create!(order_id: order.id, menu_item_id: menu_item.id, menu_item_name: menu_item.name, menu_item_price: menu_item.price)
     if params[:cart]
       redirect_to cart_path
+      flash[:notice] = "Item added successfully!"
     else
       redirect_to menus_path
+      flash[:error] = menu_item.errors.full_messages + menu.errors.full_messages
     end
   end
 
@@ -29,7 +31,10 @@ class OrderItemsController < ApplicationController
     order_item = OrderItem.find(order_item_id).destroy
     if params[:cart]
       redirect_to cart_path
+      flash[:notice] = "Item updated successfully!"
+      flash[:error] = menu_item.errors.full_messages + menu.errors.full_messages
     else
+      flash[:error] = menu_item.errors.full_messages + menu.errors.full_messages
       redirect_to menus_path
     end
   end
