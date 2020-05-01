65b04105c9edb7371c9e8a24eda50afd74c75cc5
amarnath-reddy-0-9-1-2 <122003102@sastra.ac.in>
2020-05-01 11:29:10 +0530

    menus and user sign up added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,15 @@
-class SessionsController < ApllicationController
+class SessionsController < ApplicationController
+  def new
+  end
 
+  def create
+    email = params[:email]
+    password = params[:password]
+    user = User.find_by(email: email)
+    if user.authenticate(password)
+      redirect_to root_path
+    else
+      render plain: "you entered incorrect deatails"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :menus
+  resources :menu_items
+  resources :orders
+  resources :order_items
+  resources :users
+  get "/cart", to: "orders#cart", as: "cart"
+  get "/pending_orders", to: "orders#pending_orders", as: "pending_orders"
+  get "/sign_in", to: "sessions#new", as: :new_session
+  post "/sign_in", to: "sessions#create", as: :session
 end
